0d4b9aa83890f89f693a934491b487627d57f221
Kim Ng <kimleeng@gmail.com>
2021-12-15 12:15:17 +0100

    add material ui and emotion selector

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Card>
+          <CardHeader title="How're you feeling today?" />
+          <CardContent>
+            <ButtonGroup variant="text" size="large" aria-label="large text button group">
+              <Button>😀</Button>
+              <Button>😐</Button>
+              <Button>😢</Button>
+            </ButtonGroup>
+          </CardContent>
+        </Card>
         <p>
-          Hello World
+          <Button variant="contained">Hello World</Button>
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
