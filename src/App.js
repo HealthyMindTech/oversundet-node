55ca16d7e6f91dc9bb6f54eab5017d0a38fb649e
Kim Ng <kimleeng@gmail.com>
2021-12-16 15:41:13 +0100

    modelled after pcb board;

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardContent from '@mui/material/CardContent';
+import pcb_image from './pcb.png';
+import './App.css';
+import { Button, IconButton} from '@mui/material/';
+import { Card, CardMedia, CardActionArea, Grid } from '@mui/material';
+import HelpIcon from '@mui/icons-material/Help';
+import SignalWifiConnectedNoInternet4Icon from '@mui/icons-material/SignalWifiConnectedNoInternet4';
+import SignalWifiStatusbar4BarIcon from '@mui/icons-material/SignalWifiStatusbar4Bar';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Input from '@mui/material/Input';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Button variant="contained">Connect</Button>
-        </p>
-        <Card>
-          <CardHeader title="How're you feeling today?" />
-          <CardContent>
-            <ButtonGroup variant="text" size="large" aria-label="large text button group">
-              <Button>😀</Button>
-              <Button>😐</Button>
-              <Button>😢</Button>
-            </ButtonGroup> 
-          </CardContent>
-        </Card>
-        <p>
-          <Button variant="contained">Help</Button>
-        </p>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                image={pcb_image}
+                alt="pcb"
+              />
+              <Grid container>
+                {/* if connected use */}
+                {/* <SignalWifiStatusbar4BarIcon fontSize="large" style={{ position: "absolute", top: "5px", right: "5px" }}/> */}
+                <IconButton style={{ position: "absolute", top: "5px", right: "5px" }}>
+                  <SignalWifiConnectedNoInternet4Icon fontSize="large"/>
+                </IconButton>
+                <IconButton style={{ position: "absolute", top: "55px", right: "5px" }}>
+                  <EmojiEmotionsIcon fontSize="large"/>
+                </IconButton>
+                <IconButton style={{ position: "absolute", top: "105px", right: "5px" }}>
+                  <HelpIcon fontSize="large"/>
+                </IconButton>
+              </Grid>
+            </CardActionArea>
+          </Card>
       </header>
     </div>
   );
