095981c4fb167fc1333b1d6a627c1b97149ddf1a
Kim Ng <kimleeng@gmail.com>
2021-12-20 14:46:49 +0100

    didnt use grid

--- src/App.js
+++ src/App.js
@@ -17,104 +17,98 @@ function App() {
           <Card>
             <CardActionArea>
             </CardActionArea>
-              <Grid container direction="row" spacing={2}>
-                <Box sx={{ position: 'relative' }}>
-                  <CardMedia
-                      component="img"
-                      image={pcb_image}
-                      alt="pcb"
-                  />
-      
-                  <IconButton style={{ position: "absolute", top: "3%", right: "3%" }}>
-                    <SignalWifiConnectedNoInternet4Icon fontSize="large"/>
-                  </IconButton>
-                  <Link href="/">
-                    <Box
-                      sx={{
-                        position: 'absolute',
-                        bottom: "0.5%",
-                        left: "30%",
-                        width: "30%",
-                        height: "4.8%",
-                        border: '1px solid #F00',
-                        bgcolor: 'rgba(0, 0, 0, 0.54)',
-                        color: 'white',
-                        padding: '10px',
-                      }}
-                    >
-                    </Box>
-                  </Link>
-                  
-                  <IconButton style={{ position: "absolute", top: "10%", right: "3%" }}
-                    tooltip="How are ya?">
-                    <EmojiEmotionsIcon fontSize="large"/>
-                  </IconButton>
-
-                  <Link href="/">
-                    <Box label="😀"
-                      sx={{
-                        position: 'absolute',
-                        top: "32.5%",
-                        left: "31%",
-                        width: "4.5%",
-                        height: "9%",
-                        border: '1px solid #F00',
-                        bgcolor: 'rgba(0, 0, 0, 0.54)',
-                        color: 'white',
-                        padding: '10px',
-                      }}
-                    >
-                    </Box>
-                  </Link>
-                  
-                  <Link href="/">
-                    <Box label="😐"
-                      sx={{
-                        position: 'absolute',
-                        top: "32.5%",
-                        left: "41.8%",
-                        width: "4.5%",
-                        height: "9%",
-                        border: '1px solid #F00',
-                        bgcolor: 'rgba(0, 0, 0, 0.54)',
-                        color: 'white',
-                        padding: '10px',
-                      }}
-                    >
-                    </Box>
-                  </Link>
-
-                  <Link href="/">
-                    <Box label="😥"
-                      sx={{
-                        position: 'absolute',
-                        top: "32.5%",
-                        left: "52.6%",
-                        width: "4.5%",
-                        height: "9%",
-                        border: '1px solid #F00',
-                        bgcolor: 'rgba(0, 0, 0, 0.54)',
-                        color: 'white',
-                        padding: '10px',
-                      }}
-                    >
-                    </Box>
-                  </Link>
-
-                  <Link href="https://discord.gg/45cjemQa">
-                    <IconButton style={{ position: "absolute", top: "17%", right: "3%" }}
-                      tooltip="Questions?">
-                      <HelpIcon fontSize="large"/>
-                    </IconButton>
-                  </Link>
+            <Box sx={{ position: 'relative' }}>
+              <CardMedia
+                  component="img"
+                  image={pcb_image}
+                  alt="pcb"
+              />
+  
+              <IconButton style={{ position: "absolute", top: "3%", right: "3%" }}>
+                <SignalWifiConnectedNoInternet4Icon fontSize="large"/>
+              </IconButton>
+              <Link href="/">
+                <Box
+                  sx={{
+                    position: 'absolute',
+                    bottom: "0.5%",
+                    left: "30%",
+                    width: "30%",
+                    height: "4.8%",
+                    border: '1px solid #F00',
+                    bgcolor: 'rgba(0, 0, 0, 0.54)',
+                    color: 'white',
+                    padding: '10px',
+                  }}
+                >
                 </Box>
-                {/* if connected use */}
-                {/* <SignalWifiStatusbar4BarIcon fontSize="large" style={{ position: "absolute", top: "5px", right: "5px" }}/> */}
-                
+              </Link>
+              
+              <IconButton style={{ position: "absolute", top: "10%", right: "3%" }}
+                tooltip="How are ya?">
+                <EmojiEmotionsIcon fontSize="large"/>
+              </IconButton>
 
-              </Grid>
+              <Link href="/">
+                <Box label="😀"
+                  sx={{
+                    position: 'absolute',
+                    top: "32.5%",
+                    left: "31%",
+                    width: "4.5%",
+                    height: "9%",
+                    border: '1px solid #F00',
+                    bgcolor: 'rgba(0, 0, 0, 0.54)',
+                    color: 'white',
+                    padding: '10px',
+                  }}
+                >
+                </Box>
+              </Link>
+              
+              <Link href="/">
+                <Box label="😐"
+                  sx={{
+                    position: 'absolute',
+                    top: "32.5%",
+                    left: "41.8%",
+                    width: "4.5%",
+                    height: "9%",
+                    border: '1px solid #F00',
+                    bgcolor: 'rgba(0, 0, 0, 0.54)',
+                    color: 'white',
+                    padding: '10px',
+                  }}
+                >
+                </Box>
+              </Link>
 
-            
+              <Link href="/">
+                <Box label="😥"
+                  sx={{
+                    position: 'absolute',
+                    top: "32.5%",
+                    left: "52.6%",
+                    width: "4.5%",
+                    height: "9%",
+                    border: '1px solid #F00',
+                    bgcolor: 'rgba(0, 0, 0, 0.54)',
+                    color: 'white',
+                    padding: '10px',
+                  }}
+                >
+                </Box>
+              </Link>
+
+              <Link href="https://discord.gg/45cjemQa">
+                <IconButton style={{ position: "absolute", top: "17%", right: "3%" }}
+                  tooltip="Questions?">
+                  <HelpIcon fontSize="large"/>
+                </IconButton>
+              </Link>
+            </Box>
+            {/* if connected use */}
+            {/* <SignalWifiStatusbar4BarIcon fontSize="large" style={{ position: "absolute", top: "5px", right: "5px" }}/> */}
 
           </Card>
       </header>
