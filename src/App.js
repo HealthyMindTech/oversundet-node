8049087af8f4149c492e47b404c5b6bdc2a58965
Kim Ng <kimleeng@gmail.com>
2021-12-20 14:04:38 +0100

    add location mappings

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SignalWifiConnectedNoInternet4Icon from '@mui/icons-material/SignalWifiCo
 // import SignalWifiStatusbar4BarIcon from '@mui/icons-material/SignalWifiStatusbar4Bar';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
 
+import { Box, Link } from '@mui/material';
 
 function App() {
   return (
@@ -15,29 +16,111 @@ function App() {
       <header className="App-header">
           <Card>
             <CardActionArea>
-              <CardMedia
-                component="img"
-                image={pcb_image}
-                alt="pcb"
-              />
-              <Grid container>
+            </CardActionArea>
+              <Grid container direction="row" spacing={2}>
+                <Box sx={{ position: 'relative' }}>
+                  <CardMedia
+                      component="img"
+                      image={pcb_image}
+                      alt="pcb"
+                  />
+      
+                  <IconButton style={{ position: "absolute", top: "5px", right: "5px" }}>
+                    <SignalWifiConnectedNoInternet4Icon fontSize="large"/>
+                  </IconButton>
+                  <Link href="/">
+                    <Box
+                      sx={{
+                        position: 'absolute',
+                        bottom: 5,
+                        left: 155,
+                        width: 175,
+                        height: 40,
+                        border: '1px solid #F00',
+                        bgcolor: 'rgba(0, 0, 0, 0.54)',
+                        color: 'white',
+                        padding: '10px',
+                      }}
+                    >
+                    </Box>
+                  </Link>
+                  
+                  <IconButton style={{ position: "absolute", top: "55px", right: "5px" }}
+                    tooltip="How are ya?">
+                    <EmojiEmotionsIcon fontSize="large"/>
+                  </IconButton>
+
+                  <Link href="/">
+                    <Box label="😀"
+                      sx={{
+                        position: 'absolute',
+                        top: 270,
+                        left: 167,
+                        width: 25,
+                        height: 65,
+                        border: '1px solid #F00',
+                        bgcolor: 'rgba(0, 0, 0, 0.54)',
+                        color: 'white',
+                        padding: '10px',
+                      }}
+                    >
+                    </Box>
+                  </Link>
+                  
+                  <Link href="/">
+                    <Box label="😐"
+                      sx={{
+                        position: 'absolute',
+                        top: 270,
+                        left: 224,
+                        width: 25,
+                        height: 65,
+                        border: '1px solid #F00',
+                        bgcolor: 'rgba(0, 0, 0, 0.54)',
+                        color: 'white',
+                        padding: '10px',
+                      }}
+                    >
+                    </Box>
+                  </Link>
+
+                  <Link href="/">
+                    <Box label="😥"
+                      sx={{
+                        position: 'absolute',
+                        top: 270,
+                        left: 282,
+                        width: 25,
+                        height: 65,
+                        border: '1px solid #F00',
+                        bgcolor: 'rgba(0, 0, 0, 0.54)',
+                        color: 'white',
+                        padding: '10px',
+                      }}
+                    >
+                    </Box>
+                  </Link>
+
+                  <Link href="https://discord.gg/45cjemQa">
+                    <IconButton style={{ position: "absolute", top: "105px", right: "5px" }}
+                      tooltip="Questions?">
+                      <HelpIcon fontSize="large"/>
+                    </IconButton>
+                  </Link>
+                </Box>
                 {/* if connected use */}
                 {/* <SignalWifiStatusbar4BarIcon fontSize="large" style={{ position: "absolute", top: "5px", right: "5px" }}/> */}
-                <IconButton style={{ position: "absolute", top: "5px", right: "5px" }}>
-                  <SignalWifiConnectedNoInternet4Icon fontSize="large"/>
-                </IconButton>
-                <IconButton style={{ position: "absolute", top: "55px", right: "5px" }}>
-                  <EmojiEmotionsIcon fontSize="large"/>
-                </IconButton>
-                <IconButton style={{ position: "absolute", top: "105px", right: "5px" }}>
-                  <HelpIcon fontSize="large"/>
-                </IconButton>
+                
+
               </Grid>
-            </CardActionArea>
+
+            
+
           </Card>
       </header>
     </div>
   );
 }
+
 
 export default App;
