68721d9d1a9cf23916622288da238bd8ef149133
Troels Nielsen <bn.troels@gmail.com>
2022-03-06 01:21:57 +0100

    Add auto-refresh after 10 seconds

--- src/SensorDataPlot.jsx
+++ src/SensorDataPlot.jsx
@@ -1,7 +1,43 @@
 import React, { useEffect, useState } from "react";
 import Chart from "./charts/Chart";
 import moment from 'moment';
+import constants from './constants';
 
+const refreshAll = function(deviceId, subMeasurements, granularity) {
+  const promises = subMeasurements.map(measurement => {
+    const parameters = {
+      device: deviceId,
+      measure: measurement.name,
+      interval: granularity,
+      fromTime: moment().subtract(moment.duration(granularity) * 24).utc().format('YYYY-MM-DDTHH:mm:ss[Z]'),
+      toTime: moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]'),
+    };
+    const url = new URL(constants.ONESENSOR_DATA_URL);
+    url.search = new URLSearchParams(parameters).toString();
+    
+    return fetch(url).then(response => response.json());
+  });
+  
+  return Promise.all(promises).then(data => {
+    // Add first measurement data
+    const formattedData = data[0].timestamps.map((timestamp, index) => {
+      return {
+        timestamp: new Date(timestamp),
+        [subMeasurements[0].name]: data[0].values[index]
+      }
+    });
+    // Add the rest, if they exist
+    for (let i = 1; i < data.length; i++) {
+      const measurementData = data[i];
+      measurementData.values.forEach((value, index) => {
+        formattedData[index][subMeasurements[i].name] = value;
+      });
+    }
+    return formattedData;
+  }).catch(error => {
+    console.log(error);
+  });
+}
 
 export default function SensorDataPlot (props) {
   const { measurements, granularity, deviceId, refresh } = props;
@@ -16,45 +52,25 @@ export default function SensorDataPlot (props) {
   //     periods.push(now.subtract(granularity, 'seconds').toDate());
   //   }
 
+  
+  useEffect(() => {
+    refreshAll(deviceId, subMeasurements, granularity).then(setData);
+  }, [granularity, deviceId, refresh, subMeasurements]);
 
   useEffect(() => {
-    const promises = subMeasurements.map(measurement => {
-      const parameters = {
-        device: deviceId,
-        measure: measurement.name,
-        interval: granularity,
-        fromTime: moment().subtract(moment.duration(granularity) * 24).utc().format('YYYY-MM-DDTHH:mm:ss[Z]'),
-        toTime: moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]'),
-      };
-      const url = new URL('https://oversundet-functions-apim.azure-api.net/oversundet-functions/GetOneSensor');
-      url.search = new URLSearchParams(parameters).toString();
-    
-      return fetch(url)
-        .then(response => {
-          return response.json();
-        })
-      });
-      Promise.all(promises).then(data => {
-        // Add first measurement data
-        const formattedData = data[0].timestamps.map((timestamp, index) => {
-          return {
-            timestamp,
-            [subMeasurements[0].name]: data[0].values[index]
-          }
-        });
-        // Add the rest, if they exist
-        for (let i = 1; i < data.length; i++) {
-          const measurementData = data[i];
-          measurementData.values.forEach((value, index) => {
-            formattedData[index][subMeasurements[i].name] = value;
-          });
-        }
-        setData(formattedData)
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [granularity, deviceId, refresh, subMeasurements]);
+    let timer;
+    const refreshFunc = () => {
+      refreshAll(deviceId, subMeasurements, granularity).then(setData);
+      timer = setTimeout(refreshFunc, 10000);
+    };
+
+    timer = setTimeout(refreshFunc, 10000);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [granularity, deviceId, subMeasurements, refresh]);
+  
   return (
     <Chart data={data} title={label} unit={unit} measurements={subMeasurements}/>
   )
