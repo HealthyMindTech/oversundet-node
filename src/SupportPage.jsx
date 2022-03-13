24ff3156452cf9b5204223cf3f5b66bf8b66c259
petcupaula <petcu.paula@gmail.com>
2022-03-13 18:15:16 +0100

    Beautify support page

--- src/SupportPage.jsx
+++ src/SupportPage.jsx
@@ -16,41 +16,67 @@ export default function SupportPage() {
                      </Typography>
                    </Grid>
                  </Grid>
+                 <Box width="100%" style={{margin: 20}}/>
                  <Grid container justifyContent="center">
-                   <Grid item xs={1} />
-                   <Grid item xs={5}>
+                   <Grid item xs={10}>
                      <Typography component="p" variant="p" style={{ margin: 10 }}>
                        Welcome to the support page for Extended Senses sensor collection.
                        If you are here we presume you have been given one of our sensors and are looking
-                       for assistance. First and foremost we invite you to our discord channel:
+                       for assistance.
                      </Typography>
                      <Typography component="p" variant="p" style={{margin: 10}}>
-                       <Link href="https://discord.gg/uSFYNESFke" target="_blank">
+                     First and foremost we invite you to our Discord channel: <Link href="https://discord.gg/uSFYNESFke" target="_blank">
                          https://discord.gg/uSFYNESFke
                        </Link>
                      </Typography>
                      <Typography variant="p" component="p" style={{margin: 10}}>
                        You are always welcome to ask for any problems with the sensor. Here you can find
                         a <Link target="_blank"
-                                href="discord_guide.pdf"> guide to our discord.</Link>
+                                href="discord_guide.pdf"> guide to our Discord.</Link>
                      </Typography>
                    </Grid>
-                   <Grid item xs={6}>
-                     <img src="assembly_overview.png" alt="Sensor assembly kit"/>
+                   
+                   
+                   <Box width="100%" style={{margin: 20}}/>
+                   
+                   <Grid item xs={12}>
+                     <Typography variant="h5" textAlign="center">
+                       Assembly and Setup Guide
+                     </Typography>
                    </Grid>
                    
                    <Box width="100%" style={{margin: 20}}/>
                    
-                   <Grid item xs={1} />
                    <Grid item xs={10}>
                      <Typography component="p" variant="p" style={{ margin: 10 }}>
                        A digital copy of the instruction sheet that was provided to you when you received the sensor,
                        can be found here:
                        <Link target="_blank" style={{ margin: 5 }}
-                             href="instruction_guide.pdf">Instructions</Link>
+                             href="instruction_guide.pdf">Instructions for assembly and setup</Link>
                      </Typography>
                    </Grid>
-                   <Grid item xs={1} />
+                   <Box width="100%" style={{margin: 10}}/>
+                   <Grid item xs={2}>
+                     <img src="assembly_overview_p1.png" alt="Sensor assembly kit" height="150px"/>
+                   </Grid>
+                   <Grid item xs={2}>
+                     <img src="assembly_overview_p2.png" alt="Sensor assembly kit" height="150px"/>
+                   </Grid>
+                   <Box width="100%" style={{margin: 10}}/>
+                   <Grid item xs={10}>
+                     <Typography component="p" variant="p" style={{ margin: 10 }}>
+                       The antenna that came into your kit might look a bit different than the one in the assembly instructions. 
+                       The asembly instructions are the same nevertheless. 
+                     </Typography>
+                   </Grid>
+                   <Box width="100%" style={{margin: 10}}/>
+                   <Grid item xs={10}>
+                     <Typography component="p" variant="p" style={{ margin: 10 }}>
+                       The 3d printed scaffold that came in the kit might differ. In the videos below you will see some examples
+                       and instructions on how to assemble. 
+                     </Typography>
+                   </Grid>
+                   
                    
                    <Box width="100%" style={{margin: 20}}/>
                    
@@ -59,6 +85,7 @@ export default function SupportPage() {
                        Instruction Videos
                      </Typography>
                    </Grid>
+                   <Box width="100%" style={{margin: 20}}/>
                    
                    <Grid item xs={1}></Grid>
                    <Grid item xs={2}>
