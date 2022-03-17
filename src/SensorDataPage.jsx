726f3ded1a3bd906cd83f1bb4ed88e7d62077cf8
Troels Nielsen <bn.troels@gmail.com>
2022-03-17 20:32:59 +0100

    Be more tolerant towards casing and do fewer calls

--- src/SensorDataPage.jsx
+++ src/SensorDataPage.jsx
@@ -1,15 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Grid, Paper, FormControl, Select, TextField, MenuItem, Button, Box, Typography } from '@mui/material';
-import SensorDataPlot from './SensorDataPlot';
 import constants from './constants';
 import { GithubPicker } from 'react-color';
+import Chart from "./charts/Chart";
+import moment from 'moment';
+
+const refreshAll = function(deviceId, measurements, granularity) {
+  const parameters = {
+    device: deviceId && deviceId.toLowerCase().trim(),
+    measure: measurements.flatMap(m => m.subMeasurements.map(measurement => measurement.name)).join(','),
+    interval: granularity,
+    fromTime: moment().subtract(moment.duration(granularity) * 60).utc().format('YYYY-MM-DDTHH:mm:ss[Z]'),
+    toTime: moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]'),
+  };
+  
+  const url = new URL(constants.ONESENSOR_DATA_URL);
+  url.search = new URLSearchParams(parameters).toString();
+  
+  return fetch(url).then(
+    response => response.json()
+  ).then(data => {
+    if (data.length === 0) {
+      return [];
+    }
+    const res = Object.keys(data).map(key => {
+      return data[key].map(d => ({ [key]: d }));
+    }).reduce((a, b) => a.map((v, i) => Object.assign(v, b[i]))).map(
+      (r) => Object.assign(r, { timestamp: new Date(r['timestamps']) })
+    );
+                          
+    return res;
+  }).catch(error => {
+    console.log(error);
+  });
+}
 
 export default function SensorDataPage() {
   const { urlDeviceId } = useParams();
   const [deviceId, setDeviceId] = useState(urlDeviceId);
   const [granularity, setGranularity] = useState(constants.GRANULARITY[0].value);
   const [refresh, setRefresh] = useState(0);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    let timer;
+    // Keep a cancel token to ensure that we don't update the view if timer is activated just before
+    // we are removed, but are removed at the time that all requests have come in.
+    let cancelToken = {};
+    const refreshFunc = () => {
+      if(cancelToken.cancelled) {
+        return;
+      }
+      refreshAll(deviceId, constants.MEASUREMENTS, granularity).then((data) => {
+        if (cancelToken.cancelled) {
+          return;
+        }
+        setData(data);
+      });
+      timer = setTimeout(refreshFunc, 10000);
+    };
+
+    refreshFunc();
+    return () => {
+      cancelToken.cancelled = true;
+      if (timer) {
+        clearTimeout(timer);
+      }
+    }
+  }, [granularity, deviceId, refresh]);
+  
 
   const colorChanged = (color) => {
     const rgb = color.rgb;
@@ -22,13 +82,15 @@ export default function SensorDataPage() {
       
     };
 
-    const queryDevice = encodeURIComponent(deviceId);
+    let myDeviceId = deviceId && deviceId.toLowerCase().trim();
+    const queryDevice = encodeURIComponent(myDeviceId);
     const jsonMessage = encodeURIComponent(JSON.stringify(message));
     fetch(
       `${constants.ONESENSOR_POST_MESSAGE_URL}?device=${queryDevice}&message=${jsonMessage}`,
       { method: 'POST' }
     );
   };
+
   return (
     <Box
       sx={{ width: 'auto', m: 3 }}
@@ -40,7 +102,7 @@ export default function SensorDataPage() {
             <Typography variant="h4" gutterBottom textAlign="center">
               Your Device
             </Typography>
-            <Grid container spacing={2} sx={{ mb: 2 }}>
+            <Grid container spacing={2}>
               <Grid item>
                 {/* Form to enter the sensor ID (free text) and the granularity of the data (dropdown) */}
                 <FormControl component="fieldset">
@@ -104,11 +166,11 @@ export default function SensorDataPage() {
                         height: 300,
                       }}
                     >
-                      <SensorDataPlot
-                        measurements={measurements}
-                        granularity={constants.GRANULARITY.find(option => option.value === granularity)}
-                        deviceId={deviceId}
-                        refresh={refresh}
+                      <Chart
+                        data={data}
+                        measurementConfig={measurements}
+                        measurements={measurements.subMeasurements}
+                        dateFormat={constants.GRANULARITY.find(option => option.value === granularity).dateFormat}
                       />
                     </Paper>
                   </Grid>
